8f348f10ee10a26553f85208f025468667417acd
zhongyuex <zhongyuex@users.noreply.github.com>
2021-02-22 22:26:33 +1100

    added slider demonstration

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "hello world"
+    if request.method == 'POST':
+        form = request.form
+        min1 = form['min1']
+        max1 = form['max1']
+        return render_template('index.html', display='the range was ' + min1 + ';' + max1, pageTitle='myFlaskApp')
+
+    return render_template('index.html', display='', pageTitle='myFlaskApp')
 
 if __name__ == '__main__':
     app.run(debug=True)
